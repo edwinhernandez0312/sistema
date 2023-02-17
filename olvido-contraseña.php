0e2042b7bf65566d49dce22b397c766ebe6eb424
ronaldoup <jose.duran4@unipamplona.edu.co>
2023-02-17 09:14:54 -0500

    icono

--- olvido-contraseña.php
+++ olvido-contraseña.php
@@ -8,6 +8,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
+    <link rel="shortcut icon" href="img/favicon.png" type="image/png">
+
 
     <title>Docs CILR - Olvide mi contraseña</title>
 
--- registrarse.php
+++ registrarse.php
@@ -12,6 +12,8 @@ include 'php/funciones.php';
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
+    <link rel="shortcut icon" href="img/favicon.png" type="image/png">
+
 
     <title>Docs CILR - Registro</title>
 
