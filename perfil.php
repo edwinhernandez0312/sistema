473ff052f7e0cc08abcf8ff53a2a7db888b22bf4
ronaldoup <jose.duran4@unipamplona.edu.co>
2023-02-21 16:25:10 -0500

    nav

--- perfil.php
+++ perfil.php
@@ -30,7 +30,61 @@ if (!isset($_SESSION['TIPO_USUARIO'])) {
 </head>
 
 <body>
-    <?php include 'nav.php' ?>
+
+
+    <!-- DataTales Example -->
+    <!-- <div class="card shadow mb-4">
+        <div class="card-header py-3">
+            <h6 class="m-0 font-weight-bold text-primary">DataTables Example</h6>
+        </div>
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Position</th>
+                            <th>Office</th>
+                            <th>Age</th>
+                            <th>Start date</th>
+                            <th>Salary</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div> -->
+    <div class="card shadow mb-4">
+    <div class="card-header py-3">
+        <h6 class="m-0 font-weight-bold text-primary">User Information</h6>
+    </div>
+    <div class="card-body">
+        <div class="row">
+            <div class="col-md-6">
+                <dl class="row">
+                    <dt class="col-sm-3">ID:</dt>
+                    <dd class="col-sm-9"><?php echo $_SESSION['ID_USUARIO']; ?></dd>
+
+                    <dt class="col-sm-3">User Type:</dt>
+                    <dd class="col-sm-9"><?php echo $_SESSION['TIPO_USUARIO']; ?></dd>
+
+                    <dt class="col-sm-3">Name:</dt>
+                    <dd class="col-sm-9"><?php echo $_SESSION['NOMBRE_USU']; ?></dd>
+
+                    <dt class="col-sm-3">Last Name:</dt>
+                    <dd class="col-sm-9"><?php echo $_SESSION['APELLIDOS_USU']; ?></dd>
+
+                    <dt class="col-sm-3">Email:</dt>
+                    <dd class="col-sm-9"><?php echo $_SESSION['EMAIL_USU']; ?></dd>
+                </dl>
+            </div>
+        </div>
+    </div>
+</div>
+<?php include 'nav.php' ?>
+
 </body>
 
 </html>
