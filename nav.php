f339b072f5d8e681b1337206a08adbf0797eff16
ronaldoup <jose.duran4@unipamplona.edu.co>
2023-02-21 11:10:12 -0500

    nav

--- nav.php
+++ nav.php
@@ -157,7 +157,7 @@ $nombre=$_SESSION['NOMBRE_USU'];
                         <!-- Nav Item - User Information -->
                         <li class="nav-item dropdown no-arrow">
                             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Douglas McGee</span>
+                                <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?php echo $nombre;?></span>
                                 <img class="img-profile rounded-circle" src="img/undraw_profile.svg">
                             </a>
                             <!-- Dropdown - User Information -->
@@ -226,9 +226,6 @@ $nombre=$_SESSION['NOMBRE_USU'];
                 <div class="modal-footer">
                     <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
                     <a class="btn btn-primary" href="salir.php">Logout</a>
-                    <?php 
-                    session_destroy();
-                    ?>
                 </div>
             </div>
         </div>
